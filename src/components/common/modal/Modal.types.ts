25cf957e203c5e9f23b12aea00bd341bd89b3f4c
hoon730 <ehdgns730@gmail.com>
2026-02-12 17:00:04 +0900

    docs: modal폴더 gitkeep 파일 추가

--- src/components/common/modal/Modal.types.ts
+++
@@ -1,9 +0,0 @@
-import { type ReactNode } from "react";
-
-export interface IModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  children: ReactNode;
-  title?: string;
-  size?: "sm" | "md" | "lg";
-}
